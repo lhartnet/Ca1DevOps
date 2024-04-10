3515251e6208603becbdcf17f22e3fb7d8612aff
Laura Hartnett <l00179653@atu.ie>
2024-04-10 02:37:17 +0100

    Add project files.

---
+++ Ca1DevOps/Program.cs
@@ -0,0 +1,10 @@
+using Ca1DevOps;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
